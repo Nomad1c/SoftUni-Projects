157d50ee3ff03a04dc06661b7656a5aeb8791fe6
Nomad1c <nomadic@keemail.me>
2024-01-13 16:24:13 +0200

    Updating my projects

---
+++ C#/04-ProgrammingFundamentalswithCSharp-Jan2024/01.IntroandBasicSyntaxLAB/01.StudentInformation/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StudentInformation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+            
+            string name = Console.ReadLine();
+            int age = Convert.ToInt32(Console.ReadLine());
+            double averageGrade = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Name: {name}, Age: {age}, Grade: {averageGrade:F2}");
+        }
+    }
+}
