01adaf4415551a09e58021d4a8032ff3f6425098
Nomad1c <nomadic@keemail.me>
2024-01-13 20:07:59 +0200

    Updating my projects

---
+++ C#/04-ProgrammingFundamentalswithCSharp-Jan2024/02.IntroandBasicSyntaxExercise/02.Division/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Division
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+            
+            int n = Convert.ToInt32(Console.ReadLine());
+            int counter = 0;
+
+
+            if (n % 10 == 0)
+            {
+                Console.WriteLine("The number is divisible by 10");
+                counter++;
+            }
+            else if (n % 7 == 0 && counter == 0)
+            {
+                Console.WriteLine("The number is divisible by 7");
+                counter++;
+            }
+            else if (n % 6 == 0 && counter == 0)
+            {
+                Console.WriteLine("The number is divisible by 6");
+                counter++;
+            }
+            else if (n % 3 == 0 && counter == 0)
+            {
+                Console.WriteLine("The number is divisible by 3");
+                counter++;
+            }
+            else if (n % 2 == 0 && counter == 0)
+            {
+                Console.WriteLine("The number is divisible by 2");
+                counter++;
+            }
+            else
+            {
+                Console.WriteLine("Not divisible");
+            }
+        }
+    }
+}
