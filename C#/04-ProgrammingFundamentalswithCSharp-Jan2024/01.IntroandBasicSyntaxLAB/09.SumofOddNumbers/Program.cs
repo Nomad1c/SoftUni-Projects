abbbb5597abbd46cfa58b692d76dfdf8210a3ee4
Nomad1c <nomadic@keemail.me>
2024-01-13 18:14:34 +0200

    Updating my projects

---
+++ C#/04-ProgrammingFundamentalswithCSharp-Jan2024/01.IntroandBasicSyntaxLAB/09.SumofOddNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SumofOddNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+            
+            int n = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            int i = 1;
+
+            while (i <= n)
+            {
+                sum += i * 2 - 1;
+                Console.WriteLine(i * 2 - 1);
+                i++;
+            }
+
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
