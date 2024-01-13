6de624b835133fa722f6229ba0d2b61ea92bd95d
Nomad1c <nomadic@keemail.me>
2024-01-13 20:38:00 +0200

    Updating my projects

---
+++ C#/04-ProgrammingFundamentalswithCSharp-Jan2024/02.IntroandBasicSyntaxExercise/05.Login/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string username = Console.ReadLine();
+        string correctPassword = ReverseString(username);
+
+        int attempts = 0;
+
+        while (true)
+        {
+            string passwordAttempt = Console.ReadLine();
+
+            if (passwordAttempt == correctPassword)
+            {
+                Console.WriteLine($"User {username} logged in.");
+                break;
+            }
+            else
+            {
+                attempts++;
+
+                if (attempts == 4)
+                {
+                    Console.WriteLine($"User {username} blocked!");
+                    break;
+                }
+
+                Console.WriteLine("Incorrect password. Try again.");
+            }
+        }
+    }
+
+    static string ReverseString(string input)
+    {
+        char[] charArray = input.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+}
