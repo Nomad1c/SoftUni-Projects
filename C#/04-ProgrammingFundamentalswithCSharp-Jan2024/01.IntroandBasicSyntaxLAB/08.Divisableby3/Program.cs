c3d85470138a40539addfd2ee6670303246640a7
Nomad1c <nomadic@keemail.me>
2024-01-13 18:04:53 +0200

    Updating my projects

---
+++ C#/04-ProgrammingFundamentalswithCSharp-Jan2024/01.IntroandBasicSyntaxLAB/08.Divisableby3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Divisableby3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+            
+            for (int i = 3; i <= 100; i += 3)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
