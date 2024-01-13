c151c1b2834d243dcd555344a8c2e2929bd8b5e2
Nomad1c <nomadic@keemail.me>
2024-01-13 20:29:52 +0200

    Updating my projects

---
+++ C#/04-ProgrammingFundamentalswithCSharp-Jan2024/02.IntroandBasicSyntaxExercise/04.PrintandSum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PrintandSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+            
+            int start = Convert.ToInt32(Console.ReadLine());
+            int finnish = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = start; i <= finnish; i++)
+            {
+                Console.Write($"{i} ");
+                sum += i;
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
