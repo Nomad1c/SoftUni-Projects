b9976c4f4b87d1882800eb5ccb233730cbfade16
Nomad1c <nomadic@keemail.me>
2024-01-13 17:54:02 +0200

    Updating my projects

---
+++ C#/04-ProgrammingFundamentalswithCSharp-Jan2024/01.IntroandBasicSyntaxLAB/06.ForeignLanguages/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ForeignLanguages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+            
+            string country = Console.ReadLine();
+
+            switch (country)
+            {
+                case "England":
+                case "USA":
+                    Console.WriteLine("English");
+                    break;
+                case "Spain":
+                case "Argentina":
+                case "Mexico":
+                    Console.WriteLine("Spanish");
+                    break;
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+            }
+        }
+    }
+}
