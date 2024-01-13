07fd396f02098d252c96f6f154ec0528fc63f594
Nomad1c <nomadic@keemail.me>
2024-01-13 20:00:19 +0200

    Updating my projects

---
+++ C#/04-ProgrammingFundamentalswithCSharp-Jan2024/02.IntroandBasicSyntaxExercise/01.Ages/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Ages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+            
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if (age >= 0 && age <= 2)
+            {
+                Console.WriteLine("baby");
+            }
+            else if (age >= 3 &&  age <= 13)
+            {
+                Console.WriteLine("child");
+            }
+            else if (age >= 14 && age <= 19)
+            {
+                Console.WriteLine("teenager");
+            }
+            else if (age >= 20 && age <= 65)
+            {
+                Console.WriteLine("adult");
+            }
+            else if (age >= 66)
+            {
+                Console.WriteLine("elder");
+            }
+        }
+    }
+}
