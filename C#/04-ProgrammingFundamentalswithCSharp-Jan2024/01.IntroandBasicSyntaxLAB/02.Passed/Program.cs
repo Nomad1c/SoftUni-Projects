c5cceb8aff8e625a14dc2d1900da94bc99ab2b78
Nomad1c <nomadic@keemail.me>
2024-01-13 16:28:30 +0200

    Updating my projects

---
+++ C#/04-ProgrammingFundamentalswithCSharp-Jan2024/01.IntroandBasicSyntaxLAB/02.Passed/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Passed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+            
+            double grade = Convert.ToDouble(Console.ReadLine());
+
+            if (grade > 3)
+            {
+                Console.WriteLine("Passed!");
+            }
+        }
+    }
+}
