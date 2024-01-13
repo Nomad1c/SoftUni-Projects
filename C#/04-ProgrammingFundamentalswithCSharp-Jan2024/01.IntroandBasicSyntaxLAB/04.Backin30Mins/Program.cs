5131930f2fae2c2bec1cb7362e152ee5a42eaf67
Nomad1c <nomadic@keemail.me>
2024-01-13 16:51:08 +0200

    Updating my projects

---
+++ C#/04-ProgrammingFundamentalswithCSharp-Jan2024/01.IntroandBasicSyntaxLAB/04.Backin30Mins/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Backin30Mins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+            
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int totalMinutes = (a * 60 + b) + 30;
+
+            if (totalMinutes % 1440 == 0)
+            {
+                int hours = totalMinutes / 60;
+                int minutes = totalMinutes % 60;
+
+                TimeSpan timeSpan = new TimeSpan(hours, minutes, 0);
+                string formattedTime = $"{timeSpan:hh\\:mm}";
+
+                Console.WriteLine(formattedTime);
+            }
+
+            else
+            {
+                while (totalMinutes % 1440 > 0)
+                {
+                    totalMinutes -= 1440;
+                }
+
+                int hours = totalMinutes / 60;
+                int minutes = totalMinutes % 60;
+
+                TimeSpan timeSpan = new TimeSpan(hours, minutes, 0);
+                string formattedTime = $"{timeSpan:hh\\:mm}";
+
+                Console.WriteLine(formattedTime);
+            }
+
+
+        }
+    }
+}
