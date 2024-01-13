7a665129aff999e28cd4e827571c38b3aa1d5dab
Nomad1c <nomadic@keemail.me>
2024-01-13 18:33:41 +0200

    Updating my projects

---
+++ C#/04-ProgrammingFundamentalswithCSharp-Jan2024/01.IntroandBasicSyntaxLAB/12.EvenNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EvenNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            if (n % 2 == 1)
+            {
+                while (n % 2 == 1)
+                {
+                    Console.WriteLine("Please write an even number.");
+                    n = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            int res = Math.Abs(n);
+            Console.WriteLine($"The number is: {res}");
+        }
+    }
+}
