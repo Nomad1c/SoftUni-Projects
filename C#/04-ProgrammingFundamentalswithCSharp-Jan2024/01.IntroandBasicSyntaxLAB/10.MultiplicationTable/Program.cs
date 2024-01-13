ecb7611c892b53f299f12961d04067cd56028159
Nomad1c <nomadic@keemail.me>
2024-01-13 18:20:00 +0200

    Updating my projects

---
+++ C#/04-ProgrammingFundamentalswithCSharp-Jan2024/01.IntroandBasicSyntaxLAB/10.MultiplicationTable/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+            
+            int n = Convert.ToInt32(Console.ReadLine());
+            int res = 0;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                res = n * i;
+
+                Console.WriteLine($"{n} X {i} = {res}");
+            }
+        }
+    }
+}
