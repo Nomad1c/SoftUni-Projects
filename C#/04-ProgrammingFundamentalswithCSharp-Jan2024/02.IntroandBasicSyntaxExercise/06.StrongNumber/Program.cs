5a4c8796f02a90d26921e6133700a8c825c23c10
Nomad1c <nomadic@keemail.me>
2024-01-14 18:11:34 +0200

    Updating my projects

---
+++ C#/04-ProgrammingFundamentalswithCSharp-Jan2024/02.IntroandBasicSyntaxExercise/06.StrongNumber/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace StrongNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+            
+            string input = Console.ReadLine();
+            string a = string.Empty;
+            int sum = 0;
+            int num = 0;
+            int inputNum = Convert.ToInt32(input);
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                a = input.Substring(i, 1);
+                num = Convert.ToInt32(a);
+                num = FactorialSumer(num);
+                sum += num;
+            }
+
+            if (sum == inputNum)
+            {
+                Console.WriteLine("yes");
+            }
+
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+
+        static int FactorialSumer(int a)
+        {
+            int mult = 1;
+            
+            for (int i = 1;i <= a; i++)
+            {
+                mult *= i;
+            }
+
+            return mult;
+        }
+    }
+}
