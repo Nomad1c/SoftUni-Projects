dfaddc14f0b31b72ecf7057becbd280f72849023
Nomad1c <nomadic@keemail.me>
2024-01-14 19:35:32 +0200

    Updating my projects

---
+++ C#/04-ProgrammingFundamentalswithCSharp-Jan2024/02.IntroandBasicSyntaxExercise/09.PadawanEquipment/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace PadawanEquipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+            
+            double johnMoney = Convert.ToDouble(Console.ReadLine());
+            int studentNumint = Convert.ToInt32(Console.ReadLine());
+            double saberCost = Convert.ToDouble(Console.ReadLine());
+            double robeCost = Convert.ToDouble(Console.ReadLine());
+            double beltCost = Convert.ToDouble(Console.ReadLine());
+
+            double placeholder = Convert.ToDouble(studentNumint);
+
+            placeholder = Math.Ceiling(placeholder * 1.1);
+
+            studentNumint = Convert.ToInt32(placeholder);
+            
+            double totalCost = studentNumint * (saberCost + robeCost + beltCost);
+
+            if (johnMoney >= totalCost)
+            {
+                Console.WriteLine($"The money is enough - it would cost {totalCost:F2}lv.");
+            }
+            else
+            {
+                double needed = johnMoney - totalCost;
+                Console.WriteLine($"John will need {needed:F2}lv more.");
+            }
+        }
+    }
+}
