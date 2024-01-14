6a48d42c75aecdcca6c42acaa738e20500a91bd4
Nomad1c <nomadic@keemail.me>
2024-01-14 19:15:06 +0200

    Updating my projects

---
+++ C#/04-ProgrammingFundamentalswithCSharp-Jan2024/02.IntroandBasicSyntaxExercise/08.TriangleofNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TriangleofNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+            
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write($"{j} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
