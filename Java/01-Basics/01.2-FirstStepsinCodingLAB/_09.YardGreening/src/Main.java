219337d3a0383751cf48b9d1ff82a4642b073913
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-18 14:26:16 +0200

    Updating my repo again

--- Java/01-Basics/01.2-FirstStepsinCodingLAB/_09.YardGreening/src/Main.java
+++ Java/01-Basics/01.2-FirstStepsinCodingLAB/_09.YardGreening/src/Main.java
@@ -1,15 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+class LandscapingCost {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double squareMeters = Double.parseDouble(scanner.nextLine());
+        double pricePerSquareMeter = 7.61;
+        double totalPrice = squareMeters * pricePerSquareMeter;
+        double discount = 0.18 * totalPrice;
+        double finalPrice = totalPrice - discount;
+
+        System.out.printf("The final price is: %.2f lv.%n", finalPrice);
+        System.out.printf("The discount is: %.2f lv.%n", discount);
+
+        scanner.close();
     }
 }
