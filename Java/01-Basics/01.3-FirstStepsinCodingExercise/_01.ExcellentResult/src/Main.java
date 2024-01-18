f018698262d1cdd407561edd5c60bfba90e801ed
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-18 15:07:32 +0200

    Updating my repo again

---
+++ Java/01-Basics/01.3-FirstStepsinCodingExercise/_01.ExcellentResult/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class ExcellentGrade {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int grade = Integer.parseInt(scanner.nextLine());
+
+        if (grade >= 5) {
+            System.out.println("Excellent!");
+        }
+
+        scanner.close();
+    }
+}
