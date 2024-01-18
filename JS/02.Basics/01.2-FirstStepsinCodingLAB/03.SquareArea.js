f2cefa3a536d44f3c1b6c7160bf4fa8f7305f6fa
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-18 08:30:40 +0200

    Updating my repo again

---
+++ JS/02.Basics/01.2-FirstStepsinCodingLAB/03.SquareArea.js
@@ -0,0 +1,5 @@
+function square (input) {
+    let a = input * input
+    console.log(a)
+}
+square(5)
---
+++ JS/02.Basics/01.2-FirstStepsinCodingLAB/04.InchestoCentimeters.js
@@ -0,0 +1,5 @@
+function convertor (a) {
+    let res = a * 2.54
+    console.log(res)
+}
+convertor(5.1)
