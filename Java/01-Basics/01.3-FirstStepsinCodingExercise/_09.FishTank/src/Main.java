8e27e31ba73b0f3767bb7b73dca06c5f20d07118
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-18 14:57:32 +0200

    Updating my repo again

--- Java/01-Basics/01.3-FirstStepsinCodingExercise/_09.FishTank/src/Main.java
+++ Java/01-Basics/01.3-FirstStepsinCodingExercise/_09.FishTank/src/Main.java
@@ -1,15 +1,19 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+class Aquarium {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int length = Integer.parseInt(scanner.nextLine());
+        int width = Integer.parseInt(scanner.nextLine());
+        int height = Integer.parseInt(scanner.nextLine());
+        double percentOccupied = Double.parseDouble(scanner.nextLine());
+
+        int aquariumVolume = length * width * height;
+        double litersNeeded = aquariumVolume * 0.001 * (1 - percentOccupied / 100);
+
+        System.out.println(litersNeeded);
+
+        scanner.close();
     }
 }
