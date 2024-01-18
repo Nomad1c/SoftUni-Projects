1b113a30cdea3b284436e734e3c5a0db21800af0
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-18 11:46:08 +0200

    Updating my repo again

---
+++ JS/02.Basics/02.3-ConditionalStatementsExercise/04.ToyShop.js
@@ -0,0 +1,42 @@
+function toyShop(holidayPrice, puzzles, dolls, bears, minions, trucks) {
+    let pricePuzzle = 2.6
+    let priceDoll = 3
+    let priceBear = 4.1
+    let priceMinion = 8.2
+    let priceTruck = 2
+
+    if (puzzles + dolls + bears + minions + trucks >= 50) {
+        let total = 0.9 * 0.75 * (puzzles * pricePuzzle + dolls * priceDoll + bears * priceBear + minions * priceMinion + trucks * priceTruck)
+        
+        if (total <= holidayPrice){
+            let rest = Math.abs(total - holidayPrice)
+            rest = rest.toFixed(2)
+            console.log(`Not enough money! ${rest} lv needed.`)
+        }
+
+        else {
+            let rest = Math.abs(total - holidayPrice)
+            rest = rest.toFixed(2)
+            console.log(`Yes! ${rest} lv left.`)
+        }
+    }
+
+    else {
+        let total = 0.9 * (puzzles * pricePuzzle + dolls * priceDoll + bears * priceBear + minions * priceMinion + trucks * priceTruck)
+
+        if (total <= holidayPrice){
+            let rest = Math.abs(total - holidayPrice)
+            rest = rest.toFixed(2)
+            console.log(`Not enough money! ${rest} lv needed.`)
+        }
+
+        else {
+            let rest = Math.abs(total - holidayPrice)
+            rest = rest.toFixed(2)
+            console.log(`Yes! ${rest} lv left.`)
+        }
+    }
+}
+
+toyShop(40.8, 20, 25, 30, 50, 10)
+toyShop(320, 8, 2, 5, 5, 1)
