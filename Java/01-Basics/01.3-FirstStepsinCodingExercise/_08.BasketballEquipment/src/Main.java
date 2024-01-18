18e39c23ec96cceeeaf42c2201ff0894e65dea80
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-18 14:55:17 +0200

    Updating my repo again

--- Java/01-Basics/01.3-FirstStepsinCodingExercise/_08.BasketballEquipment/src/Main.java
+++ Java/01-Basics/01.3-FirstStepsinCodingExercise/_08.BasketballEquipment/src/Main.java
@@ -1,15 +1,20 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+class BasketballEquipment {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int annualFee = Integer.parseInt(scanner.nextLine());
+
+        double sneakersPrice = annualFee - 0.4 * annualFee;
+        double outfitPrice = sneakersPrice - 0.2 * sneakersPrice;
+        double ballPrice = 0.25 * outfitPrice;
+        double accessoriesPrice = 0.2 * ballPrice;
+
+        double totalCost = annualFee + sneakersPrice + outfitPrice + ballPrice + accessoriesPrice;
+
+        System.out.println(totalCost);
+
+        scanner.close();
     }
 }
