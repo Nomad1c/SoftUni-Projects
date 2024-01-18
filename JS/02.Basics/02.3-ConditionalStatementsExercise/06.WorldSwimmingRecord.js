6e8e181fac600ba2d95f3c7b61e79f97b1a52955
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-18 12:00:23 +0200

    Updating my repo again

---
+++ JS/02.Basics/02.3-ConditionalStatementsExercise/06.WorldSwimmingRecord.js
@@ -0,0 +1,18 @@
+function swimmingRecord(record, distance, timePerMeter) {
+    record = parseFloat(record);
+    distance = parseFloat(distance);
+    timePerMeter = parseFloat(timePerMeter);
+
+    let resistance = Math.floor(distance / 15) * 12.5;
+    let totalTime = (distance * timePerMeter) + resistance;
+
+    if (totalTime < record) {
+        console.log(`Yes, he succeeded! The new world record is ${totalTime.toFixed(2)} seconds.`);
+    } else {
+        let timeDifference = totalTime - record;
+        console.log(`No, he failed! He was ${timeDifference.toFixed(2)} seconds slower.`);
+    }
+}
+
+swimmingRecord("10464", "1500", "20");
+swimmingRecord("55555.67", "3017", "5.03");
