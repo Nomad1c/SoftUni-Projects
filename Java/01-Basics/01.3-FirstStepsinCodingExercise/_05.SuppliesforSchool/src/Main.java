797107e7e3e6c3fc98d9893d68ffbcfd33542608
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-18 14:49:45 +0200

    Updating my repo again

--- Java/01-Basics/01.3-FirstStepsinCodingExercise/_05.SuppliesforSchool/src/Main.java
+++ Java/01-Basics/01.3-FirstStepsinCodingExercise/_05.SuppliesforSchool/src/Main.java
@@ -1,15 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+class SchoolSupplies {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int pencilsCount = Integer.parseInt(scanner.nextLine());
+        int markersCount = Integer.parseInt(scanner.nextLine());
+        double cleaningSolutionLiters = Double.parseDouble(scanner.nextLine());
+        int discountPercent = Integer.parseInt(scanner.nextLine());
+
+        double pencilsPrice = pencilsCount * 5.80;
+        double markersPrice = markersCount * 7.20;
+        double cleaningSolutionPrice = cleaningSolutionLiters * 1.20;
+        double total = pencilsPrice + markersPrice + cleaningSolutionPrice;
+
+        double discount = total * (discountPercent / 100.0);
+        double finalPrice = total - discount;
+
+        System.out.println(finalPrice);
+
+        scanner.close();
     }
 }
