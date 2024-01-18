dd939d61e0ef13cb52545f1a32d5d2145b2523eb
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-18 14:51:21 +0200

    Updating my repo again

--- Java/01-Basics/01.3-FirstStepsinCodingExercise/_06.Repainting/src/Main.java
+++ Java/01-Basics/01.3-FirstStepsinCodingExercise/_06.Repainting/src/Main.java
@@ -1,15 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+class Painting {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int nylonSquareMeters = Integer.parseInt(scanner.nextLine());
+        int paintLiters = Integer.parseInt(scanner.nextLine());
+        int thinnerLiters = Integer.parseInt(scanner.nextLine());
+        int hoursOfWork = Integer.parseInt(scanner.nextLine());
+
+        double nylonPrice = (nylonSquareMeters + 2) * 1.50;
+        double paintPrice = (paintLiters + paintLiters * 0.10) * 14.50;
+        double thinnerPrice = thinnerLiters * 5.00;
+        double bagsPrice = 0.40;
+
+        double totalMaterialsCost = nylonPrice + paintPrice + thinnerPrice + bagsPrice;
+        double laborCost = (totalMaterialsCost * 0.30) * hoursOfWork;
+
+        double totalCost = totalMaterialsCost + laborCost;
+
+        System.out.printf("%.2f%n", totalCost);
+
+        scanner.close();
     }
 }
