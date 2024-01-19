e34ce3093874a6836605f1d8bc9340fa0b26605d
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-19 14:24:04 +0200

    Updating my repo again

---
+++ C#/02-Fund/04.DataTypesandVariablesLAB/06.ReversedChars/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ReversedChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+
+            char char1 = char.Parse(Console.ReadLine());
+            char char2 = char.Parse(Console.ReadLine());
+            char char3 = char.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{char3} {char2} {char1}");
+        }
+    }
+}
