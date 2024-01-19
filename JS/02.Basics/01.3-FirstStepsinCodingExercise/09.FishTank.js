f5401e0cf1144ae5182be722a4e07459036e0c2c
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-19 09:06:25 +0200

    Updating my repo again

--- JS/02.Basics/01.3-FirstStepsinCodingExercise/09.FishTank.js
+++ JS/02.Basics/01.3-FirstStepsinCodingExercise/09.FishTank.js
@@ -1,10 +1,10 @@
-function calculateAquariumVolume(length, width, height, percentage) {
-    const aquariumVolume = (length * width * height) / 1000;
-    const occupiedSpace = percentage / 100;
+function calculateAquariumVolume(data) {
+    const aquariumVolume = (data[0] * data[1] * data[2]) / 1000;
+    const occupiedSpace = data[3] / 100;
     const neededLiters = aquariumVolume * (1 - occupiedSpace);
 
     console.log(neededLiters);
 }
 
-calculateAquariumVolume(85, 75, 47, 17);
-calculateAquariumVolume(105, 77, 89, 18.5);
+calculateAquariumVolume([85, 75, 47, 17]);
+calculateAquariumVolume([105, 77, 89, 18.5]);
--- JS/02.Basics/02.2-ConditionalStatementsLAB/02.GreaterNumber.js
+++ JS/02.Basics/02.2-ConditionalStatementsLAB/02.GreaterNumber.js
@@ -1,6 +1,6 @@
-function findGreaterNumber(firstNumber, secondNumber) {
-    let num1 = Number(firstNumber);
-    let num2 = Number(secondNumber);
+function findGreaterNumber(data) {
+    let num1 = Number(data[0]);
+    let num2 = Number(data[1]);
 
     if (num1 > num2) {
         console.log(num1);
@@ -9,7 +9,7 @@ function findGreaterNumber(firstNumber, secondNumber) {
     }
 }
 
-findGreaterNumber("5", "3");
-findGreaterNumber("3", "5");
-findGreaterNumber("10", "10");
-findGreaterNumber("-5", "5");
+findGreaterNumber(["5", "3"]);
+findGreaterNumber(["3", "5"]);
+findGreaterNumber(["10", "10"]);
+findGreaterNumber(["-5", "5"]);
