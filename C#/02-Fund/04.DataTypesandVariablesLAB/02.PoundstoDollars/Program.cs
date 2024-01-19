bf19beb8ba8f287ad5762d234ca48727d66cca75
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-19 10:39:57 +0200

    Updating my repo again

---
+++ C#/02-Fund/04.DataTypesandVariablesLAB/02.PoundstoDollars/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PoundstoDollars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+
+            double pounds = Convert.ToDouble(Console.ReadLine());
+            double dollars = pounds * 1.31;
+            Console.WriteLine($"{dollars:F3}");
+        }
+    }
+}
