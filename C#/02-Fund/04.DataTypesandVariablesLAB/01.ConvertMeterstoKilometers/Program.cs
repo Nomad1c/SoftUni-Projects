3b828317f0d2b856a0e2fd178e5dea7c8aa895c8
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-19 10:34:46 +0200

    Updating my repo again

--- C#/02-Fund/04.DataTypesandVariablesLAB/01.ConvertMeterstoKilometers/Program.cs
+++ C#/02-Fund/04.DataTypesandVariablesLAB/01.ConvertMeterstoKilometers/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ConvertMeterstoKilometers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+            
+            int meters = Convert.ToInt32(Console.ReadLine());
+            double km = Convert.ToDouble(meters) / 1000;
+
+            Console.WriteLine($"{km:F2}");
+        }
+    }
+}
