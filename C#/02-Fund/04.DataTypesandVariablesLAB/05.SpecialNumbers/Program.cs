6f24e7c03a08dc39f966cd5c3c4219b1535e4ef3
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-19 12:24:28 +0200

    Updating my repo again

---
+++ C#/02-Fund/04.DataTypesandVariablesLAB/05.SpecialNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SpecialNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                bool isSpecial = IsSpecialNumber(i);
+                Console.WriteLine($"{i} -> {isSpecial}");
+            }
+
+        }
+
+        static bool IsSpecialNumber(int number)
+        {
+            int sumOfDigits = 0;
+
+            while (number > 0)
+            {
+                sumOfDigits += number % 10;
+                number /= 10;
+            }
+
+            return sumOfDigits == 5 || sumOfDigits == 7 || sumOfDigits == 11;
+        }
+    }
+}
