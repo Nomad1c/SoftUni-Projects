ea036ab243a20e4774f81f9a8f9496b821accad0
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-19 14:31:05 +0200

    Updating my repo again

---
+++ C#/02-Fund/04.DataTypesandVariablesLAB/10.LowerorUpper/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LowerorUpper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+            char inputChar = char.Parse(Console.ReadLine());
+
+            if (char.IsUpper(inputChar))
+            {
+                Console.WriteLine("upper-case");
+            }
+            else
+            {
+                Console.WriteLine("lower-case");
+            }
+        }
+    }
+}
