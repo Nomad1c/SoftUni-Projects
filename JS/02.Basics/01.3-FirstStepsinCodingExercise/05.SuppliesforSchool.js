4a29da6fc424a237245aa366355a3c6f5714df8a
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-19 08:49:22 +0200

    Updating my repo again

--- JS/02.Basics/01.3-FirstStepsinCodingExercise/05.SuppliesforSchool.js
+++ JS/02.Basics/01.3-FirstStepsinCodingExercise/05.SuppliesforSchool.js
@@ -1,9 +1,9 @@
-function suplies (pens, markers, litre, percent) {
-    let sum = pens * 5.8 + markers * 7.2 + litre * 1.2
-    let discount = sum * percent / 100
+function suplies (data) {
+    let sum = data[0] * 5.8 + data[1] * 7.2 + data[2] * 1.2
+    let discount = sum * data[3] / 100
     let total = sum - discount
 
     console.log(total)
 }
-suplies(2, 3, 4, 25)
-suplies(4, 2, 5, 13)
+suplies([2, 3, 4, 25])
+suplies([4, 2, 5, 13])
