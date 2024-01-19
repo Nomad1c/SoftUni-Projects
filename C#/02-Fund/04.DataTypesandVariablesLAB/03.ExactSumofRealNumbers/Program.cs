e33cdf6073fbdfb46648fa2064f3944c0ddb82d3
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-19 12:18:26 +0200

    Updating my repo again

---
+++ C#/02-Fund/04.DataTypesandVariablesLAB/03.ExactSumofRealNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ExactSumofRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+            
+            int n = Convert.ToInt32(Console.ReadLine());
+            decimal sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                sum += Convert.ToDecimal(Console.ReadLine());
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ C#/02-Fund/04.DataTypesandVariablesLAB/04.CenturiestoMinutes/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace CenturiestoMinutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+            
+            byte senturies = Convert.ToByte(Console.ReadLine());
+            short years = (short)(senturies * 100);
+            int days = (int)(years * 365.2422);
+            long hours = (long)(days * 24);
+            double minutes = (double)(hours * 60);
+
+            Console.WriteLine(years);
+            Console.WriteLine(days);
+            Console.WriteLine(hours);
+            Console.WriteLine(minutes);
+        }
+    }
+}
