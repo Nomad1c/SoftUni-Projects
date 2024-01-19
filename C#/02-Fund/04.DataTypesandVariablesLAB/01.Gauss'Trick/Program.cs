6ae7ec2f16dc8a758eddba4bdf10aa448665067f
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-19 15:21:11 +0200

    Updating my repo again

---
+++ C#/02-Fund/04.DataTypesandVariablesLAB/01.Gauss'Trick/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GaussTrick
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (int i = 0; i < numbers.Length / 2; i++)
+            {
+                int sum = numbers[i] + numbers[numbers.Length - 1 - i];
+                Console.Write(sum + " ");
+            }
+
+            if (numbers.Length % 2 != 0)
+            {
+                Console.Write(numbers[numbers.Length / 2]);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
