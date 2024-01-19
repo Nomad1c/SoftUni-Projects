27fa29b67e39440de078ae23de982d46faf3e2ce
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-19 14:32:54 +0200

    Updating my repo again

---
+++ C#/02-Fund/04.DataTypesandVariablesLAB/11.RefactorVolumeofPyramid/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RefactorVolumeofPyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+
+            Console.WriteLine("Length: ");
+            double length = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Width: ");
+            double width = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Height: ");
+            double height = double.Parse(Console.ReadLine());
+
+            double volume = (length * width * height) / 3;
+
+            Console.WriteLine($"Pyramid Volume: {volume:f2}");
+        }
+    }
+}
