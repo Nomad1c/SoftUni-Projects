6e87680a2115cbeaa5ecaf55a0653b8b91bed2bb
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-19 09:08:55 +0200

    Updating my repo again

--- JS/02.Basics/02.2-ConditionalStatementsLAB/07.AreaofFigures.js
+++ JS/02.Basics/02.2-ConditionalStatementsLAB/07.AreaofFigures.js
@@ -1,25 +1,25 @@
-function calculateArea(figure, param1, param2) {
+function calculateArea(data) {
     let area = 0;
 
-    switch (figure) {
+    switch (data[0]) {
         case "square":
-            area = param1 * param1;
+            area = data[1] * data[1];
             break;
         case "rectangle":
-            area = param1 * param2;
+            area = data[1] * data[2];
             break;
         case "circle":
-            area = Math.PI * param1 * param1;
+            area = Math.PI * data[1] * data[1];
             break;
         case "triangle":
-            area = (param1 * param2) / 2;
+            area = (data[1] * data[2]) / 2;
             break;
     }
 
     return area.toFixed(3);
 }
 
-console.log(calculateArea("square", 5));
-console.log(calculateArea("rectangle", 7, 2.5));
-console.log(calculateArea("circle", 6));
-console.log(calculateArea("triangle", 4.5, 20));
+console.log(calculateArea(["square", 5]));
+console.log(calculateArea(["rectangle", 7, 2.5]));
+console.log(calculateArea(["circle", 6]));
+console.log(calculateArea(["triangle", 4.5, 20]));
