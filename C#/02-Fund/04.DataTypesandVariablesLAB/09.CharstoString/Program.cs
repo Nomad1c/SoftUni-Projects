1bed18c8544c90499b22b8fa0e40b22a0b9d2a89
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-19 14:29:42 +0200

    Updating my repo again

---
+++ C#/02-Fund/04.DataTypesandVariablesLAB/09.CharstoString/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CharstoString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+
+            char char1 = char.Parse(Console.ReadLine());
+            char char2 = char.Parse(Console.ReadLine());
+            char char3 = char.Parse(Console.ReadLine());
+
+            string result = $"{char1}{char2}{char3}";
+            Console.WriteLine(result);
+        }
+    }
+}
