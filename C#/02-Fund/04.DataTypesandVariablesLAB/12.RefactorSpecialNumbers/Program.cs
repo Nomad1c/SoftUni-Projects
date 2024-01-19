d8ea0e5f18becd075be937e33cabfd63b651463f
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-19 14:34:46 +0200

    Updating my repo again

---
+++ C#/02-Fund/04.DataTypesandVariablesLAB/12.RefactorSpecialNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace RefactorSpecialNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int num = 1; num <= n; num++)
+            {
+                int currentNum = num;
+                int sumOfDigits = 0;
+
+                while (currentNum > 0)
+                {
+                    sumOfDigits += currentNum % 10;
+                    currentNum /= 10;
+                }
+
+                bool isSpecialNum = (sumOfDigits == 5) || (sumOfDigits == 7) || (sumOfDigits == 11);
+
+                Console.WriteLine($"{num} -> {isSpecialNum}");
+            }
+        }
+    }
+}
