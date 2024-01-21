9518506c7cc596a172d040e2c024fe8ea730cad5
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-21 17:46:15 +0200

    Updating my repo

---
+++ QA/04-Back-EndTechnologiesBasics/05.JS-Basics-Excersise/07.CharstoString.js
@@ -0,0 +1,20 @@
+function conc (a, b, c) {
+    
+    let x = String(a)
+    let y = String(b)
+    let z = String(c)
+
+    console.log(x+y+z)
+}
+conc('a',
+'b',
+'c'
+)
+conc('%',
+'2',
+'o'
+)
+conc('1',
+'5',
+'p'
+)
