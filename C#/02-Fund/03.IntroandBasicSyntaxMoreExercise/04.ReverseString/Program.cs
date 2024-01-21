8a29b8d55869329020d81b5b256f9ae83d9c73e4
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-21 19:21:56 +0200

    Updating my repo

---
+++ C#/02-Fund/03.IntroandBasicSyntaxMoreExercise/04.ReverseString/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+
+            string input = Console.ReadLine();
+            string reversed = ReverseString(input);
+            Console.WriteLine(reversed);
+        }
+
+        static string ReverseString(string str)
+        {
+            char[] charArray = str.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
