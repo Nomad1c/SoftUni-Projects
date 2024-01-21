184670c9c2909f2d7ce2c5ff55491b4104e9d11c
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-21 17:51:06 +0200

    Updating my repo

---
+++ QA/04-Back-EndTechnologiesBasics/05.JS-Basics-Excersise/08.ReversedChars.js
@@ -0,0 +1,16 @@
+function conc (a, b, c) {
+    
+    let x = String(c)
+    let y = String(b)
+    let z = String(a)
+
+    console.log(`${x} ${y} ${z}`)
+}
+conc('A',
+'B',
+'C'
+)
+conc('1',
+'L',
+'&'
+)
