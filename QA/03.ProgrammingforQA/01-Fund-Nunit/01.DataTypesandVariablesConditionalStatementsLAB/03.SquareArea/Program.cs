e82e93804c4f8bf063332cba863241d1302946c9
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-24 09:42:15 +0200

    Updating my repo again

---
+++ QA/03.ProgrammingforQA/01-Fund-Nunit/01.DataTypesandVariablesConditionalStatementsLAB/03.SquareArea/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SquareArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+            
+            int a = Convert.ToInt32(Console.ReadLine());
+            a *= a;
+            Console.WriteLine(a);
+        }
+    }
+}
