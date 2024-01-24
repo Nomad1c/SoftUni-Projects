622a40e203cbb647b6a9004aa47e69c71753ab64
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-24 09:37:22 +0200

    Updating my repo again

---
+++ QA/03.ProgrammingforQA/01-Fund-Nunit/01.DataTypesandVariablesConditionalStatementsLAB/01.HelloSoftUni/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloSoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+
+            Console.WriteLine("Hello SoftUni!");
+        }
+    }
+}
