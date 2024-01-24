a52c2a8a63a28bc9f955d76e1558da28f52f95a8
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-24 12:36:21 +0200

    Updating my repo again

---
+++ QA/03.ProgrammingforQA/01-Fund-Nunit/02.DataTypesCalculationsExercise/05.TeachingMaterials/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TeachingMaterials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+
+            int pensCount = int.Parse(Console.ReadLine());
+            int markersCount = int.Parse(Console.ReadLine());
+            int boardCleanerLiters = int.Parse(Console.ReadLine());
+            int discountPercentage = int.Parse(Console.ReadLine());
+
+            double pensPrice = pensCount * 5.80;
+            double markersPrice = markersCount * 7.20;
+            double boardCleanerPrice = boardCleanerLiters * 1.20;
+
+            double totalCost = pensPrice + markersPrice + boardCleanerPrice;
+            double discountAmount = totalCost * (discountPercentage / 100.0);
+            double finalPrice = totalCost - discountAmount;
+
+            Console.WriteLine(finalPrice);
+        }
+    }
+}
+
