466a047081f1d85cac36bc903f8283f621adf397
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-24 12:42:19 +0200

    Updating my repo again

---
+++ QA/03.ProgrammingforQA/01-Fund-Nunit/02.DataTypesCalculationsExercise/07.FoodDelivery/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FoodDelivery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+
+            int chickenMenus = int.Parse(Console.ReadLine());
+            int fishMenus = int.Parse(Console.ReadLine());
+            int vegetarianMenus = int.Parse(Console.ReadLine());
+
+            double chickenMenuPrice = chickenMenus * 10.35;
+            double fishMenuPrice = fishMenus * 12.40;
+            double vegetarianMenuPrice = vegetarianMenus * 8.15;
+
+            double totalMenuCost = chickenMenuPrice + fishMenuPrice + vegetarianMenuPrice;
+
+            double dessertPrice = totalMenuCost * 0.20;
+            double deliveryPrice = 2.50;
+
+            double totalOrderPrice = totalMenuCost + dessertPrice + deliveryPrice;
+
+            Console.WriteLine(totalOrderPrice);
+        }
+    }
+}
