8354d21fc8a52eb378811ddd1b32bf4b74bbe444
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-24 11:00:49 +0200

    Updating my repo again

---
+++ QA/03.ProgrammingforQA/01-Fund-Nunit/02.DataTypesCalculationsExercise/03.DepositCalculator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DepositCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+
+            double depositedAmount = double.Parse(Console.ReadLine());
+            int termOfDeposit = int.Parse(Console.ReadLine());
+            double annualInterestRate = double.Parse(Console.ReadLine());
+
+            double accumulatedInterest = depositedAmount * annualInterestRate / 100;
+            double monthlyInterest = accumulatedInterest / 12;
+            double totalAmount = depositedAmount + termOfDeposit * monthlyInterest;
+
+            Console.WriteLine($"{totalAmount:F2}");
+        }
+    }
+}
