47379cfd8153f02e3d84ec7c58d2c1780b71d175
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-24 12:32:49 +0200

    Updating my repo again

---
+++ QA/03.ProgrammingforQA/01-Fund-Nunit/02.DataTypesCalculationsExercise/04.MandatoryLiterature/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MandatoryLiterature
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+
+            int totalPages = int.Parse(Console.ReadLine());
+            int pagesPerHour = int.Parse(Console.ReadLine());
+            int numberOfDays = int.Parse(Console.ReadLine());
+
+            int totalReadingTime = totalPages / pagesPerHour;
+            int hoursPerDay = totalReadingTime / numberOfDays;
+
+            Console.WriteLine(hoursPerDay);
+        }
+    }
+}
