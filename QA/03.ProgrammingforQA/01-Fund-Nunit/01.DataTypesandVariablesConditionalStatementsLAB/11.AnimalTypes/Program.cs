b8ddeae6a0d9d5be93a88ac81a1c729c0947dc7e
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-24 10:34:15 +0200

    Updating my repo again

---
+++ QA/03.ProgrammingforQA/01-Fund-Nunit/01.DataTypesandVariablesConditionalStatementsLAB/11.AnimalTypes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AnimalTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+
+            string animal = Console.ReadLine();
+
+            ClassifyAnimal(animal);
+        }
+
+        static void ClassifyAnimal(string animal)
+        {
+            switch (animal.ToLower())
+            {
+                case "dog":
+                    Console.WriteLine("mammal");
+                    break;
+                case "crocodile":
+                case "tortoise":
+                case "snake":
+                    Console.WriteLine("reptile");
+                    break;
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+            }
+        }
+    }
+}
