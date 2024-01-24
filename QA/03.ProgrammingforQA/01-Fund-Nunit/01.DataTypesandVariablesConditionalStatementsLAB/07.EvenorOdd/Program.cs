49e61e3df6204d569f3d47a17a469828733ae69c
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-24 10:12:42 +0200

    Updating my repo again

---
+++ QA/03.ProgrammingforQA/01-Fund-Nunit/01.DataTypesandVariablesConditionalStatementsLAB/07.EvenorOdd/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EvenorOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+            
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Even");
+            }
+            else
+            {
+                Console.WriteLine("Odd");
+            }
+        }
+    }
+}
