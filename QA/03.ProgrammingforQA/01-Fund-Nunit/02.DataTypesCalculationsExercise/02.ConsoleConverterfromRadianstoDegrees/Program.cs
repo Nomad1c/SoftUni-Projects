38e238484d274367cdb84dc0b463237afc4d0171
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-24 10:57:05 +0200

    Updating my repo again

---
+++ QA/03.ProgrammingforQA/01-Fund-Nunit/02.DataTypesCalculationsExercise/02.ConsoleConverterfromRadianstoDegrees/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleConverterformRadianstoDegrees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+
+            double radians = double.Parse(Console.ReadLine());
+
+            double degrees = radians * 180 / Math.PI;
+
+            Console.WriteLine(degrees);
+        }
+    }
+}
