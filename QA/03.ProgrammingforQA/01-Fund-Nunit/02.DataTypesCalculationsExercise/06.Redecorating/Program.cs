55836f05a0adbb38691c746a0c74cc30d62cc2d1
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-24 12:39:49 +0200

    Updating my repo again

---
+++ QA/03.ProgrammingforQA/01-Fund-Nunit/02.DataTypesCalculationsExercise/06.Redecorating/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Redecorating
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+
+            int nylonSqMeters = int.Parse(Console.ReadLine());
+            int paintLiters = int.Parse(Console.ReadLine());
+            int thinnerLiters = int.Parse(Console.ReadLine());
+            int hoursOfWork = int.Parse(Console.ReadLine());
+
+            double nylonCost = (nylonSqMeters + 2) * 1.50;
+            double paintCost = (paintLiters + (paintLiters * 0.10)) * 14.50;
+            double thinnerCost = thinnerLiters * 5.00;
+            double bagsCost = 0.40;
+
+            double totalMaterialCost = nylonCost + paintCost + thinnerCost + bagsCost;
+
+            double craftsmenCost = (totalMaterialCost * 0.30) * hoursOfWork;
+
+            double totalCost = totalMaterialCost + craftsmenCost;
+
+            Console.WriteLine(totalCost);
+        }
+    }
+}
