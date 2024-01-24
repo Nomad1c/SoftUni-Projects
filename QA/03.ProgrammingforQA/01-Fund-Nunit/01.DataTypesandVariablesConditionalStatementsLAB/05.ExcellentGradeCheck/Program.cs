f8a2fe20c4a88b047425fb8fb4bf3fbafe525e55
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-24 09:50:50 +0200

    Updating my repo again

---
+++ QA/03.ProgrammingforQA/01-Fund-Nunit/01.DataTypesandVariablesConditionalStatementsLAB/05.ExcellentGradeCheck/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ExcellentGradeCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+            
+            double grade = Convert.ToDouble(Console.ReadLine());
+
+            if (grade > 5.5)
+            {
+                Console.WriteLine("Excelent!");
+            }
+        }
+    }
+}
