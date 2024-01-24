26a3b5fc1cf2e9da629ff66ddfc3910f5ba75720
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-24 09:53:21 +0200

    Updating my repo again

---
+++ QA/03.ProgrammingforQA/01-Fund-Nunit/01.DataTypesandVariablesConditionalStatementsLAB/06.GradesIfElse/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GradesIfElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+            
+            double grade = Convert.ToDouble(Console.ReadLine());
+
+            if (grade > 5.5)
+            {
+                Console.WriteLine("Excelent!");
+            }
+            else
+            {
+                Console.WriteLine("Not excelent.");
+            }
+        }
+    }
+}
