e5c3235f994a15eaac176b807edcb808a8f07e01
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-24 09:47:54 +0200

    Updating my repo again

---
+++ QA/03.ProgrammingforQA/01-Fund-Nunit/01.DataTypesandVariablesConditionalStatementsLAB/04.InchestoCentimetersConverter/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace InchestoCentimetersConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+            
+            double inch = Convert.ToDouble(Console.ReadLine());
+            double cm = inch * 2.54;
+            Console.WriteLine(cm);
+        }
+    }
+}
