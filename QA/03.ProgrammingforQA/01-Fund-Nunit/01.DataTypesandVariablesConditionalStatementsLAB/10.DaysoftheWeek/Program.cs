8aa3f8282712c9e6ee0a9bcf3110a331a7a77b25
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-24 10:31:23 +0200

    Updating my repo again

---
+++ QA/03.ProgrammingforQA/01-Fund-Nunit/01.DataTypesandVariablesConditionalStatementsLAB/10.DaysoftheWeek/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace DaysoftheWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+            
+            int day = int.Parse(Console.ReadLine());
+
+            ConvertToDayName(day);
+        }
+
+        static void ConvertToDayName(int day)
+        {
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+                default:
+                    Console.WriteLine("Error");
+                    break;
+            }
+        }
+    }
+}
