225bb3b9e145c2f7b9df7560eeb9d36a0452f8e6
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-24 10:17:05 +0200

    Updating my repo again

---
+++ QA/03.ProgrammingforQA/01-Fund-Nunit/01.DataTypesandVariablesConditionalStatementsLAB/08.TheGreaterNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TheGreaterNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+            
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                Console.WriteLine($"Greater number: {num1}");
+            }
+
+            else if (num1 < num2)
+            {
+                Console.WriteLine($"Greater number: {num2}");
+            }
+
+            else
+            {
+                Console.WriteLine("The two numbers are equal.");
+            }
+        }
+    }
+}
