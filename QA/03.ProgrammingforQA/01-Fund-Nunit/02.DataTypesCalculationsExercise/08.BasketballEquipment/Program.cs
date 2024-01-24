fb4e34ccbdbc4b44e1b903b270885eba7d5ef901
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-24 12:45:08 +0200

    Updating my repo again

---
+++ QA/03.ProgrammingforQA/01-Fund-Nunit/02.DataTypesCalculationsExercise/08.BasketballEquipment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BasketballEquipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+
+            int trainingFee = int.Parse(Console.ReadLine());
+
+            double sneakersPrice = trainingFee - (0.4 * trainingFee);
+            double teamPrice = sneakersPrice - (0.2 * sneakersPrice);
+            double basketballPrice = teamPrice / 4;
+            double accessoriesPrice = basketballPrice / 5;
+
+            double totalExpense = trainingFee + sneakersPrice + teamPrice + basketballPrice + accessoriesPrice;
+
+            Console.WriteLine(totalExpense);
+        }
+    }
+}
