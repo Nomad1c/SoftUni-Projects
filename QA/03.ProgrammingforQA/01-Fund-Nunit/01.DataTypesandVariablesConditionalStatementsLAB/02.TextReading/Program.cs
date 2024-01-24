a4e456012b721fff99286f3225674d46ae8155ca
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-24 09:40:07 +0200

    Updating my repo again

---
+++ QA/03.ProgrammingforQA/01-Fund-Nunit/01.DataTypesandVariablesConditionalStatementsLAB/02.TextReading/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TextReading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+            
+            string text = Console.ReadLine();
+            Console.WriteLine(text);
+        }
+    }
+}
