7624239f3ad1c7e34c4bdc95e5493d7936fbe93d
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-24 10:49:51 +0200

    Updating my repo again

---
+++ QA/03.ProgrammingforQA/01-Fund-Nunit/02.DataTypesCalculationsExercise/01.ConsoleConverterUSDtoBGN/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleConverterUSDtoBGN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+
+            double usdAmount = double.Parse(Console.ReadLine());
+
+            double exchangeRate = 1.79549;
+            double bgnAmount = usdAmount * exchangeRate;
+
+            Console.WriteLine($"Converted amount to BGN: {bgnAmount:F5}");
+        }
+    }
+}
