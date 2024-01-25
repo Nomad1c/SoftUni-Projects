c7e86f0ef6775643363df9ef94ed316b43249369
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-25 09:10:33 +0200

    Updating my repo again

--- C#/02-Fund/05.DataTypesandVariablesExercise/01.IntegerOperations/Program.cs
+++ C#/02-Fund/05.DataTypesandVariablesExercise/01.IntegerOperations/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace IntegerOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+            
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            int d = Convert.ToInt32(Console.ReadLine());
+
+            int sum = (((a + b) / c) * d);
+
+            Console.WriteLine(sum);
+        }
+    }
+}
