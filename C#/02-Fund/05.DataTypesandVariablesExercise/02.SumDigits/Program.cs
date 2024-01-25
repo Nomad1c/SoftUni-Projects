efc7500b0078bf75c27a2d80990d14cc07a12189
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-25 13:07:49 +0200

    Updating my repo again

---
+++ C#/02-Fund/05.DataTypesandVariablesExercise/02.SumDigits/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SumDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+            
+            string a = Console.ReadLine();
+            int sum = 0;
+
+            List<int> list = new List<int>();
+
+            list = a.Split("").Select(int.Parse).ToList();
+
+            foreach (int i in list)
+            {
+                sum += i;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
