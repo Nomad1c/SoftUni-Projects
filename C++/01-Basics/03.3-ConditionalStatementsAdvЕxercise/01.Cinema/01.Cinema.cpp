e10dcb8b97553b3746890ce0802009a129f3279d
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-26 10:28:51 +0200

    Updating my repo again

---
+++ C++/01-Basics/03.3-ConditionalStatementsAdvЕxercise/01.Cinema/01.Cinema.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+int main() {
+    const double PREMIERE_PRICE = 12.00;
+    const double NORMAL_PRICE = 7.50;
+    const double DISCOUNT_PRICE = 5.00;
+
+    string projectionType;
+    int rows, columns;
+    cin >> projectionType >> rows >> columns;
+
+    double totalIncome = 0.0;
+
+    if (projectionType == "Premiere") {
+        totalIncome = PREMIERE_PRICE * rows * columns;
+    }
+    else if (projectionType == "Normal") {
+        totalIncome = NORMAL_PRICE * rows * columns;
+    }
+    else if (projectionType == "Discount") {
+        totalIncome = DISCOUNT_PRICE * rows * columns;
+    }
+
+    cout << fixed << setprecision(2) << totalIncome << " leva" << endl;
+
+    return 0;
+}
