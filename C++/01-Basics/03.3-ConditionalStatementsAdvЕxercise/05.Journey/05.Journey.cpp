2e5b8ff191d6f88e793a5f84830a9c6bd1aa78c1
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-26 11:08:44 +0200

    Updating my repo again

---
+++ C++/01-Basics/03.3-ConditionalStatementsAdvЕxercise/05.Journey/05.Journey.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    double budget;
+    string season;
+    cin >> budget >> season;
+
+    string destination;
+    string vacationType;
+    double expenses;
+
+    if (budget <= 100) {
+        destination = "Bulgaria";
+        if (season == "summer") {
+            vacationType = "Camp";
+            expenses = 0.30 * budget;
+        }
+        else if (season == "winter") {
+            vacationType = "Hotel";
+            expenses = 0.70 * budget;
+        }
+    }
+    else if (budget <= 1000) {
+        destination = "Balkans";
+        if (season == "summer") {
+            vacationType = "Camp";
+            expenses = 0.40 * budget;
+        }
+        else if (season == "winter") {
+            vacationType = "Hotel";
+            expenses = 0.80 * budget;
+        }
+    }
+    else {
+        destination = "Europe";
+        vacationType = "Hotel";
+        expenses = 0.90 * budget;
+    }
+
+    cout << "Somewhere in " << destination << endl;
+    cout << vacationType << " - " << fixed << setprecision(2) << expenses << endl;
+
+    return 0;
+}
