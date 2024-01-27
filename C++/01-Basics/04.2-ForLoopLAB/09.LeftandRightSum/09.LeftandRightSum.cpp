b865efcf9de9035b63a08b50abf311ae828e8708
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-27 17:06:10 +0200

    Updating my repo

---
+++ C++/01-Basics/04.2-ForLoopLAB/09.LeftandRightSum/09.LeftandRightSum.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int leftSum = 0;
+    int rightSum = 0;
+
+    for (int i = 0; i < n; ++i) {
+        int number;
+        cin >> number;
+        leftSum += number;
+    }
+
+    for (int i = 0; i < n; ++i) {
+        int number;
+        cin >> number;
+        rightSum += number;
+    }
+
+    if (leftSum == rightSum) {
+        cout << "Yes, sum = " << leftSum << endl;
+    }
+    else {
+        int diff = abs(leftSum - rightSum);
+        cout << "No, diff = " << diff << endl;
+    }
+
+    return 0;
+}
