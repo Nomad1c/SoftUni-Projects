8a74c46e1090baa8dbd714dc757106a02b47f9bf
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-28 23:02:50 +0200

    Updating my repo

---
+++ QA/04.BackEndTechnologiesBasics/06.JS-Fundamentals-LAB/01.SumFirstandLastArrayElements.js
@@ -0,0 +1,8 @@
+function sumFirstAndLast(arr) {
+    const sum = arr[0] + arr[arr.length - 1];
+    console.log(sum);
+}
+
+sumFirstAndLast([20, 30, 40]);
+sumFirstAndLast([10, 17, 22, 33]);
+sumFirstAndLast([11, 58, 69]);
