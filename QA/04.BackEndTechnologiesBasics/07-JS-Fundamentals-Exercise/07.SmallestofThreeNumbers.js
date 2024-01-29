d961a817ef6ff700076a00cb9768f4f35e389819
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-29 16:29:45 +0200

    Updating my repo

---
+++ QA/04.BackEndTechnologiesBasics/07-JS-Fundamentals-Exercise/07.SmallestofThreeNumbers.js
@@ -0,0 +1,9 @@
+function findSmallestNumber(num1, num2, num3) {
+    const smallestNumber = Math.min(num1, num2, num3);
+    console.log(smallestNumber);
+}
+
+findSmallestNumber(2, 5, 3);
+findSmallestNumber(600, 342, 123);
+findSmallestNumber(25, 21, 4);
+findSmallestNumber(2, 2, 2);
