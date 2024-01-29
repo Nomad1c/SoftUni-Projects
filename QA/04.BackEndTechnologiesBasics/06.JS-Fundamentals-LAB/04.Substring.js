4f783632e3cae37bb6b0223ed32292f106c3f053
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-29 16:03:59 +0200

    Updating my repo

---
+++ QA/04.BackEndTechnologiesBasics/06.JS-Fundamentals-LAB/04.Substring.js
@@ -0,0 +1,7 @@
+function substringFromString(inputString, startIndex, count) {
+    const result = inputString.substr(startIndex, count);
+    console.log(result);
+}
+
+substringFromString('ASentence', 1, 8);
+substringFromString('SkipWord', 4, 7);
