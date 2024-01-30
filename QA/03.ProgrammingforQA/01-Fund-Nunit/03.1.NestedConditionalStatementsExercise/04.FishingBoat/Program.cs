09e76f618e4b1dd6c6019112dc782022780ddd65
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-30 17:03:02 +0200

    Updating my repo

---
+++ QA/03.ProgrammingforQA/01-Fund-Nunit/03.1.NestedConditionalStatementsExercise/04.FishingBoat/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+
+            int groupBudget = int.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+            int fishermenCount = int.Parse(Console.ReadLine());
+
+            double springPrice = 3000;
+            double summerAutumnPrice = 4200;
+            double winterPrice = 2600;
+
+            double baseCost = 0;
+
+            switch (season)
+            {
+                case "Spring":
+                    baseCost = springPrice;
+                    break;
+                case "Summer":
+                case "Autumn":
+                    baseCost = summerAutumnPrice;
+                    break;
+                case "Winter":
+                    baseCost = winterPrice;
+                    break;
+                default:
+                    Console.WriteLine("Invalid season.");
+                    return;
+            }
+
+            if (fishermenCount <= 6)
+            {
+                baseCost *= 0.9;
+            }
+            else if (fishermenCount <= 11)
+            {
+                baseCost *= 0.85;
+            }
+            else
+            {
+                baseCost *= 0.75;
+            }
+
+            if (fishermenCount % 2 == 0 && season != "Autumn")
+            {
+                baseCost *= 0.95;
+            }
+
+            double moneyLeft = groupBudget - baseCost;
+
+            if (moneyLeft >= 0)
+            {
+                Console.WriteLine($"Yes! You have {moneyLeft:F2} leva left.");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money! You need {Math.Abs(moneyLeft):F2} leva.");
+            }
+        }
+    }
+}
