0087afb016359a379cbe4bbc8dd10dc2f3e59c83
Nomad1c <svetlin-dimitrov@outlook.com>
2024-01-30 16:50:48 +0200

    Updating my repo

---
+++ QA/03.ProgrammingforQA/01-Fund-Nunit/03.1.NestedConditionalStatementsExercise/01.Cinema/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Cinema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+            
+            string type = Console.ReadLine();
+            int rows = Convert.ToInt32(Console.ReadLine());
+            int columns = Convert.ToInt32(Console.ReadLine());
+            double result = 0;
+
+            if (type == "Premiere")
+            {
+                result = rows * columns * 12;
+            }
+            else if (type == "Normal")
+            {
+                result = rows * columns * 7.5;
+            }
+            else if (type == "Discount")
+            {
+                result = rows * columns * 5;
+            }
+
+            Console.WriteLine($"{result:F2} leva");
+        }
+    }
+}
