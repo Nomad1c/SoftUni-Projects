43eb72749f5d8df36ef45d38489d9ed57a5e2d72
Nomad1c <svetlin-dimitrov@outlook.com>
2024-02-10 18:19:54 +0200

    updating my repo

---
+++ C++/01-Basics/04.3-ForLoopExercise/04.CleverLily/04.CleverLily.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    int age;
+    double washingMachinePrice, toyPrice;
+
+    cin >> age;
+    cin >> washingMachinePrice;
+    cin >> toyPrice;
+
+    double savedMoney = 0.0;
+
+    for (int i = 1; i <= age; ++i) {
+        if (i % 2 == 1) {
+            savedMoney += toyPrice;
+        }
+        else {
+            savedMoney += (((i / 2.0) * 10.00) - 1.0);
+        }
+    }
+
+    if (savedMoney >= washingMachinePrice) {
+        double remainingMoney = savedMoney - washingMachinePrice;
+        printf("Yes! %.2f\n", remainingMoney);
+    }
+    else {
+        double neededMoney = washingMachinePrice - savedMoney;
+        printf("No! %.2f\n", neededMoney);
+    }
+
+    return 0;
+}
