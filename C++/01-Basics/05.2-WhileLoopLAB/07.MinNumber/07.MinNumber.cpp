d02b560d59ea5156917edd2be546f5893a919cc4
Nomad1c <svetlin-dimitrov@outlook.com>
2024-02-15 09:48:14 +0200

    updating my repo

---
+++ C++/01-Basics/05.2-WhileLoopLAB/07.MinNumber/07.MinNumber.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <limits>
+
+using namespace std;
+
+int main() {
+    int currentNumber;
+    int minNumber = numeric_limits<int>::max();
+
+    while (true) {
+        cin >> currentNumber;
+
+        if (cin.eof() || currentNumber == 0) {
+            break;
+        }
+
+        if (currentNumber < minNumber) {
+            minNumber = currentNumber;
+        }
+    }
+
+    cout << minNumber << endl;
+
+    return 0;
+}
