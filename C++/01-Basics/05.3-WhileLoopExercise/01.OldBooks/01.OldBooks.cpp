c477f97735df9f7768423c253e6a280e6f711e43
Nomad1c <svetlin-dimitrov@outlook.com>
2024-02-17 13:25:34 +0200

    updating my repo

--- C++/01-Basics/05.3-WhileLoopExercise/01.OldBooks/01.OldBooks.cpp
+++ C++/01-Basics/05.3-WhileLoopExercise/01.OldBooks/01.OldBooks.cpp
@@ -1,20 +1,31 @@
-// 01.OldBooks.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿#include <iostream>
+#include <string>
 
-#include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+    string favoriteBook;
+    getline(cin, favoriteBook);
+
+    string currentBook;
+    int checkedBooks = 0;
+
+    while (true) {
+        getline(cin, currentBook);
+
+        if (currentBook == "No More Books") {
+            cout << "The book you search is not here!" << endl;
+            cout << "You checked " << checkedBooks << " books." << endl;
+            break;
+        }
+
+        checkedBooks++;
+
+        if (currentBook == favoriteBook) {
+            cout << "You checked " << checkedBooks - 1 << " books and found it." << endl;
+            break;
+        }
+    }
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
