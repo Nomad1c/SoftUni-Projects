16aa2f14d11008bc53041fe84ef94d3de00c2a35
Nomad1c <svetlin-dimitrov@outlook.com>
2024-02-18 14:29:53 +0200

    updating my repo

---
+++ C#/02-Fund/99.EXAM/01.BurgerBus/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class BurgerBus
+{
+    public static void Main(string[] args)
+    {
+        int numCities = int.Parse(Console.ReadLine());
+
+        double totalProfit = 0;
+        double profit = 0;
+
+        for (int i = 1; i <= numCities; i++)
+        {
+            string cityName = Console.ReadLine();
+            double income = double.Parse(Console.ReadLine());
+            double expenses = double.Parse(Console.ReadLine());
+
+            profit = income;
+
+            if (i % 3 == 0 && i % 5 != 0)
+            {
+                expenses *= 1.5; // Special event
+            }
+            else if (i % 5 == 0)
+            {
+                profit *= 0.9; // Raining
+            }
+
+            profit -= expenses;
+            totalProfit += profit;
+
+            Console.WriteLine($"In {cityName} Burger Bus earned {profit:0.00} leva.");
+        }
+
+        Console.WriteLine($"Burger Bus total profit: {totalProfit:0.00} leva.");
+    }
+}
