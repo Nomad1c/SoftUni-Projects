66556a7e4fba21e363c2d089bc457cf7867e1eaa
Nomad1c <nomadic.logins@tuta.io>
2024-03-15 15:23:35 +0200

    adding programs

---
+++ C#/02-Fund/05.DataTypesandVariablesExercise/04.SumofCharts/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SumofCharts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+            
+
+        }
+    }
+}
