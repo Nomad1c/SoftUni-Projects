96d3a728116e648b408abf9610b750788dc4d732
Nomad1c <nomadic.logins@tuta.io>
2024-03-15 15:20:59 +0200

    adding programs

---
+++ C#/02-Fund/05.DataTypesandVariablesExercise/03.Elevator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+            
+            int n = Convert.ToInt32(Console.ReadLine());
+            int p = Convert.ToInt32(Console.ReadLine());
+
+            int courses = Convert.ToInt32(Math.Floor((double)n/p));
+
+            if (courses > 0 )
+            {
+                Console.WriteLine($"{courses} courses * {p} people");
+            }
+
+            else
+            {
+                Console.WriteLine("All the persons fit inside in the elevator.");
+                Console.WriteLine("Only one course is needed.");
+            }
+
+            int rest = n % p;
+
+            if ( rest != 0 )
+            {
+                Console.WriteLine($"+ 1 courcse * {rest} people");
+            }
+        }
+    }
+}
