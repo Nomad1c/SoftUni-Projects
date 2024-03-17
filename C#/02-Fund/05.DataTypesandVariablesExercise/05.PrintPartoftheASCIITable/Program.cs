f254fd725c8454e3bf5c71490a908f925ba281d4
Nomad1c <nomadic.logins@tuta.io>
2024-03-17 18:31:55 +0200

    adding programs

---
+++ C#/02-Fund/05.DataTypesandVariablesExercise/05.PrintPartoftheASCIITable/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PrintASCII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+
+            int startIndex = Convert.ToInt32(Console.ReadLine());
+            int endIndex = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = startIndex; i <= endIndex; i++)
+            {
+                char character = (char)i;
+                Console.Write(character + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ C#/02-Fund/05.DataTypesandVariablesExercise/06.TripplesofLatinLetters/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TripplesofLatinLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+            
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                char one = (char)('a' + i);
+                
+                for (int j = 0; j < n; j++)
+                {
+                    char two = (char)('a' + j);
+
+                    for (int k = 0; k < n; k++)
+                    {
+                        char tre = (char)('a' + k);
+
+                        string sum = ($@"{one}{two}{tre}");
+
+                        Console.WriteLine(sum);
+                    }
+                }
+            }
+        }
+    }
+}
