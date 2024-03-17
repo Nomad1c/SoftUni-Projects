a77e28eb05e4984fc59bd9234a33bf504c927194
Nomad1c <nomadic.logins@tuta.io>
2024-03-17 19:07:53 +0200

    adding programs

---
+++ C#/02-Fund/05.DataTypesandVariablesExercise/08.BeerKegs/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BeerKegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+            
+            int n = Convert.ToInt32(Console.ReadLine());
+            double biggestVolume = 0;
+            string biggestName = String.Empty;
+
+            for (int i = 0; i < n; i++)
+            {
+                string model = Console.ReadLine();
+                double radius = Convert.ToDouble(Console.ReadLine());
+                double height = Convert.ToDouble(Console.ReadLine());
+
+                double volume = Math.PI * radius * radius * height;
+
+                if (volume > biggestVolume)
+                {
+                    biggestVolume = volume;
+                    biggestName = model;
+                }
+            }
+
+            Console.WriteLine(biggestName);
+        }
+    }
+}
