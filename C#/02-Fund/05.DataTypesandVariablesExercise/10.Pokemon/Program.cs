c0fe6dab338ba9a99838e3bdaf0f719e0b81357b
Nomad1c <nomadic.logins@tuta.io>
2024-03-17 19:37:31 +0200

    adding programs

---
+++ C#/02-Fund/05.DataTypesandVariablesExercise/10.Pokemon/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Pokemon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+
+            int pokePower = Convert.ToInt32(Console.ReadLine());
+            int distance = Convert.ToInt32(Console.ReadLine());
+            int exhaustionFactor = Convert.ToInt32(Console.ReadLine());
+
+            int Poked = 0;
+            double originalPokePower = (double)pokePower;
+
+            while (pokePower >= distance)
+            {
+                Poked++;
+                pokePower -= distance;
+
+                if (pokePower == Math.Floor(originalPokePower / 2.0))
+                {
+                    if (pokePower % exhaustionFactor == 0)
+                    {
+                        pokePower /= exhaustionFactor;
+                    }
+                }
+            }
+
+            Console.WriteLine(pokePower);
+            Console.WriteLine(Poked);
+        }
+    }
+}
