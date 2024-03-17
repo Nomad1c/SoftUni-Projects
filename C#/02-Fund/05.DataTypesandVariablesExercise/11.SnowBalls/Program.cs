6124d1c3289f26b6d2a0ef1474e9abd8285a0f00
Nomad1c <nomadic.logins@tuta.io>
2024-03-17 19:54:09 +0200

    adding programs

---
+++ C#/02-Fund/05.DataTypesandVariablesExercise/11.SnowBalls/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SnowBalls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+            
+            int n = Convert.ToInt32(Console.ReadLine());
+            double winner = 0.0;
+            int a = 0; int b = 0; double c = 0; int d = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int snowballSnow = Convert.ToInt32(Console.ReadLine());
+                int snowballTime = Convert.ToInt32(Console.ReadLine());
+                int snowballQuality = Convert.ToInt32(Console.ReadLine());
+
+                double snowballValue = Math.Pow((double)(snowballSnow / snowballTime), (double)snowballQuality);
+                
+                if (snowballValue > winner)
+                {
+                    winner = snowballValue;
+                    a = snowballSnow;
+                    b = snowballTime;
+                    c = snowballValue;
+                    d = snowballQuality;
+                }
+            }
+
+            Console.WriteLine($@"{a} : {b} = {c} ({d})");
+        }
+    }
+}
