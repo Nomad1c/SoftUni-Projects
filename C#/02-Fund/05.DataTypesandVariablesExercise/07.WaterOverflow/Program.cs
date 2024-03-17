42e07b2edd08707d9731c8528fa68add03e112a1
Nomad1c <nomadic.logins@tuta.io>
2024-03-17 18:40:38 +0200

    adding programs

---
+++ C#/02-Fund/05.DataTypesandVariablesExercise/07.WaterOverflow/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace WaterOverflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+
+            /*
+             * water tank capacity = 255 liters
+             * ---
+             * INPUT 1 = n
+             * INPUT 2 = liters of water to pour into the tank
+             * if capacity not enough - "Insufficient capacity!"
+             * and continue reading next line
+             * on the lat line print the liters in the tank
+             */
+
+            int n = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int liters = Convert.ToInt32(Console.ReadLine());
+
+                if (sum + liters <= 255)
+                {
+                    sum += liters;
+                }
+
+                else
+                {
+                    Console.WriteLine(@"Insufficient capacity!");
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
