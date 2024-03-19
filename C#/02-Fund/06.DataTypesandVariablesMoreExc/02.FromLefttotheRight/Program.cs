a65a3af3fd6d90fbde4f73711ae7bc349f42b335
Nomad1c <nomadic.logins@tuta.io>
2024-03-19 12:38:22 +0200

    adding programs

---
+++ C#/02-Fund/06.DataTypesandVariablesMoreExc/02.FromLefttotheRight/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace FromLefttotheRight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+            
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string nums = Console.ReadLine();
+                List<double> list = new List<double>();
+                int sum = 0;
+
+                list = nums.Split(' ').Select(double.Parse).ToList();
+
+                if (list[0] > list[1])
+                {
+                    string String = Convert.ToString(list[0]);
+
+                    for (int j = 0; j < String.Length; j++)
+                    {
+                        sum += Convert.ToInt32(String.Substring(j, 1));
+                    }
+
+                    Console.WriteLine(sum);
+                    sum = 0;
+                }
+
+                else
+                {
+                    string String = Convert.ToString(list[1]);
+
+                    for (int j = 0; j < String.Length; j++)
+                    {
+                        sum += Convert.ToInt32(String.Substring(j, 1));
+                    }
+
+                    Console.WriteLine(sum);
+                    sum = 0;
+                }
+            }
+        }
+    }
+}
