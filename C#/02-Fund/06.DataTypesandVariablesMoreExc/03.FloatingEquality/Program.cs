36d216af93a93177e69e9ee7d84f3ac1b8e3829c
Nomad1c <nomadic.logins@tuta.io>
2024-03-19 15:22:40 +0200

    adding programs

---
+++ C#/02-Fund/06.DataTypesandVariablesMoreExc/03.FloatingEquality/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FloatingEquality
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+            
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            a *= 100000; b *= 100000;
+
+            int inta = Convert.ToInt32(a);
+            int intb = Convert.ToInt32(b);
+
+            bool result = inta == intb;
+
+            Console.WriteLine(result);
+        }
+    }
+}
