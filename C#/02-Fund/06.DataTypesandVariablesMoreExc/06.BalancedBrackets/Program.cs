cd7bdda7e897bbcdf9127c900ae962cab1172007
Nomad1c <nomadic.logins@tuta.io>
2024-03-20 15:17:27 +0200

    adding programs

---
+++ C#/02-Fund/06.DataTypesandVariablesMoreExc/06.BalancedBrackets/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace BalancedBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+            
+            int n = Convert.ToInt32(Console.ReadLine());
+            bool search = false;
+
+            for (int i = 0; i < n; i++)
+            {
+                string text = Console.ReadLine();
+
+                if (text == "(")
+                {
+                    search = true;
+                }
+
+                if (text == ")" && search == true)
+                {
+                    search = false;
+                }
+
+            }
+
+            if (search == false)
+            {
+                Console.WriteLine("BALANCED");
+            }
+
+            else
+            {
+                Console.WriteLine("UNBALANCED");
+            }
+        }
+    }
+}
