e205d39bcd0dd6ddb080a9c697f14668790b6d56
Nomad1c <nomadic.logins@tuta.io>
2024-03-20 14:16:31 +0200

    adding programs

---
+++ C#/02-Fund/06.DataTypesandVariablesMoreExc/04.Refactoring-PrimeChecker/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace RefractoringPrimeChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+            
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 2; i <= n; i++)
+            {
+                bool isPrime = true;
+
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                Console.WriteLine($"{i} -> {isPrime}");
+            }
+        }
+    }
+}
