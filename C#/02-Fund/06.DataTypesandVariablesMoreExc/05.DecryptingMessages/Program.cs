c6fc1b810c99c3fc466050fe20f79d612ad1ecd7
Nomad1c <nomadic.logins@tuta.io>
2024-03-20 14:29:06 +0200

    adding programs

---
+++ C#/02-Fund/06.DataTypesandVariablesMoreExc/05.DecryptingMessages/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DecryptingMessages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //The entry point for where our program begins
+        { //Anything between these curly braces is the *Body* of the main method
+            
+            int key = Convert.ToInt32(Console.ReadLine());
+            int n = Convert.ToInt32(Console.ReadLine());
+            string word = String.Empty;
+
+            for (int i = 0; i < n; i++)
+            {
+                char a = Convert.ToChar(Console.ReadLine());
+                
+                for (int j = 0; j < key; j++)
+                {
+                    a++;
+                }
+                word += a;
+            }
+
+            Console.WriteLine(word);
+        }
+    }
+}
